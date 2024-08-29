0ff4585192a6c6604870a5f1a742c2778efd3cc8
lapatatedouce59 <momo29004@gmail.com>
2024-08-29 23:10:08 +0200

    Interface primaire

---
+++ index.js
@@ -0,0 +1,314 @@
+let get = (id)=>{
+    return document.getElementById(id)
+}
+
+let canvas = get('canvas_simu')
+let ctx = canvas.getContext("2d");
+
+let delta = 1;
+let lastUpdate = Date.now();
+let max_tps = 50.0;
+
+let fuStart=false
+
+const maxThrottle = 5
+const maxSpeed = 80;
+
+let currentSpeed = 0;
+let currentDistance = 0;
+let currentPOS = 0;
+
+let currentThrottle=parseInt(get('train_throttle_input').value)
+let currentSlope=parseInt(get('train_slope_input').value)
+let currentPower=parseInt(get('train_power_input').value)
+let currentMasse=parseInt(get('train_mass_input').value)
+let currentFrottForce=parseInt(get('train_frott_input').value)
+let currentBrakePow=parseFloat(get('train_brake_input').value)
+let currentPatinage=parseFloat(get('train_pat_input').value)
+let currentBrakePowToEqPow=currentBrakePow*520/100;
+
+init()
+function init(){
+    update();
+    requestAnimationFrame(init);
+}
+
+function update(){
+    let rn = Date.now();
+    let inter = rn - lastUpdate;
+    let theorical_inter = 1000.0 / max_tps;
+    delta = inter / theorical_inter;
+    lastUpdate = rn;
+    if(delta>5)delta=5;
+    if(delta<=0)return;
+
+    CANVAS_UPDATE()
+    HTML_UPDATE()
+    SPEED_UPDATE()
+    DISTANCE_UPDATE()
+    FU_LISTENER()
+    if(get('enable_sound').checked){
+        window.updateSounds()
+    } else {
+        if(typeof SOUND_MANAGER === 'object'){
+            SOUND_MANAGER.stopAllSound()
+        }
+    }
+}
+
+let fuBtn=false
+
+function FU_LISTENER(){
+    if(fuStart===true){
+        get('btn_embrake').disabled=true
+        get('btn_embrake_reset').disabled=false
+        if(currentSpeed>0){
+            get('train_throttle_input').value=0
+            currentSpeed += (((-5.6)*((0.52/currentMasse)*1.4)));
+        } else {
+            if(currentSpeed<0){
+                currentSpeed=0
+            }
+            fuStart=fuBtn
+        }
+    }
+}
+
+function HTML_UPDATE(){
+    get('physics_toggle_state').innerText=`${get('enable_physics').checked?"Oui":"Non"}`
+    for(let elem of document.getElementsByName('auto_physics')){
+        if(elem.checked){
+            get('physics_control_state').innerText=`${elem.getAttribute("data-auto_physics")}`
+        }
+    }
+    get('train_slope_state').innerText=`${get('train_slope_input').value}`
+    get('train_power_state').innerText=`${get('train_power_input').value}`
+    get('train_brake_state').innerText=`${get('train_brake_input').value}`
+    get('train_mass_state').innerText=`${get('train_mass_input').value}`
+    get('train_frott_state').innerText=`${get('train_frott_input').value}`
+    get('train_pat_state').innerText=`${get('train_pat_input').value}`
+    for(let elem of document.getElementsByName('auto_control')){
+        if(elem.checked){
+            get('train_autocontrol_state').innerText=`${elem.getAttribute("data-auto_control")}`
+        }
+    }
+    get('train_throttle_state').innerText=`${get('train_throttle_input').value}`
+    get('vaff_enable_state').innerText=`${get('enable_vaff').checked?"Oui":"Non"}`
+    get('train_departure_toggle_state').innerText=`${get('enable_depart').checked?"Oui":"Non"}`
+    get('train_sound_state').innerText=`${get('enable_sound').checked?"Oui":"Non"}`
+
+    if(get('enable_physics').checked){
+        for(let elem of document.getElementsByName('auto_physics')){
+            elem.disabled=false
+        }
+    } else {
+        for(let elem of document.getElementsByName('auto_physics')){
+            elem.disabled=true
+        }
+    }
+    for(let elem of document.getElementsByName('auto_physics')){
+        if(elem.checked){
+            if(elem.getAttribute("data-auto_physics")==="Manu"){
+                for(let elem2 of document.getElementsByClassName('input_phy')){
+                    elem2.disabled=false
+                }
+            } else {
+                for(let elem2 of document.getElementsByClassName('input_phy')){
+                    elem2.disabled=true
+                }
+            }
+        }
+        if (get('enable_physics').checked===false || elem.getAttribute("data-auto_physics")==="Auto") {
+            for(let elem2 of document.getElementsByClassName('input_phy')){
+                elem2.disabled=true
+            }
+        }
+    }
+    for(let elem of document.getElementsByName('auto_control')){
+        if(elem.checked){
+            if(elem.getAttribute("data-auto_control")==="Manu"){
+                get('train_throttle_input').disabled=false
+            } else {
+                get('train_throttle_input').disabled=true
+            }
+        }
+    }
+    if(get('enable_depart').checked){
+        get('btn_train_departure').disabled=false
+    } else {
+        get('btn_train_departure').disabled=true
+    }
+
+    get('accel_cmd_vit_input_state').innerText=`${get('accel_cmd_vit_input').value}`
+    get('decel_cmd_vit_input_state').innerText=`${get('decel_cmd_vit_input').value}`
+
+    get('consigne_vit_input_state').innerText=`${get('consigne_vit_input').value} km/h`
+    get('consigne_pmd_input_state').innerText=`PM ${currentPOS.toFixed(2)}`
+    get('consigne_courbe_prog_input_state').innerText=`${get('consigne_courbe_prog_input').checked?"Oui":"Non"}`
+    for(let elem of document.getElementsByName('radio_lim_type_cons')){
+        if(elem.checked){
+            get('radio_lim_type_cons_state').innerText=`${elem.getAttribute("data-cons_lim")}`
+        }
+    }
+    get('consigne_aide_distance_input_state').innerText=`${get('consigne_aide_distance_input').checked?"Actif":"Non"}`
+    get('consigne_dist_input_state').innerText=`${get('consigne_dist_input').value} m`
+    get('consigne_paflim_input_state').innerText=`PM ${get('consigne_paflim_input').value}`
+    get('consigne_accel_input_state').innerText=`${get('consigne_accel_input').value} m/s²`
+
+    get('consigne_dist_input').disabled=!get('consigne_aide_distance_input').checked;
+    for(let elem of document.getElementsByName('radio_lim_type_cons')){
+        if(elem.checked){
+            if(elem.getAttribute("data-cons_lim")==="Dist"){
+                get('consigne_aide_distance_input').disabled=false
+                get('consigne_dist_input').disabled=false
+                get('consigne_paflim_input').disabled=false
+                get('consigne_accel_input').disabled=true
+            } else {
+                get('consigne_aide_distance_input').disabled=true
+                get('consigne_dist_input').disabled=true
+                get('consigne_paflim_input').disabled=true
+                get('consigne_accel_input').disabled=false
+            }
+        }
+    }
+
+    currentThrottle=parseInt(get('train_throttle_input').value)
+    currentSlope=parseInt(get('train_slope_input').value)
+    currentPower=parseInt(get('train_power_input').value)
+    currentMasse=parseInt(get('train_mass_input').value)
+    currentFrottForce=parseFloat(get('train_frott_input').value)
+    currentBrakePow=parseFloat(get('train_brake_input').value)
+    currentPatinage=parseFloat(get('train_pat_input').value)
+    currentBrakePowToEqPow=currentBrakePow*520/100;
+}
+const BasicPhysicAccel=0.016714285714285716
+function SPEED_UPDATE(){
+    let calculatedPower = 0;
+    if(currentThrottle>0){
+        calculatedPower=currentPower
+    } else if(currentThrottle<=0){
+        calculatedPower=Math.max(currentBrakePowToEqPow,currentPower)
+    }
+    let accelForce = ((calculatedPower/1000)/currentMasse)*0.9
+    let frottforce = currentFrottForce/100
+    if(get('enable_physics').checked===false) accelForce=BasicPhysicAccel
+    currentSpeed += ((currentThrottle*accelForce));
+    if((currentThrottle===0 || calculatedPower<=1) && get('enable_physics').checked) currentSpeed=currentSpeed-(frottforce*1.2)
+
+    if(currentSpeed > maxSpeed) currentSpeed = maxSpeed;
+    if(currentSpeed < 0) currentSpeed = 0;
+
+    get('actualTrainSpeed').innerText=currentSpeed.toFixed(2)
+    get('actualTrainSpeedMS').innerText=(currentSpeed/3.6).toFixed(2)
+}
+
+let lastDistance=0;
+let lastSpeed=0;
+
+setInterval(()=>{
+    let deltadist = currentDistance-lastDistance;
+    lastDistance=currentDistance;
+    
+    get('actualTrainSpeedVKMH').innerText=(deltadist*3.6*4).toFixed(2)
+    get('deltaSpeed').innerText=`${Math.abs((deltadist*3.6*4)-currentSpeed).toFixed(2)}`
+    if(Math.abs((deltadist*3.6*4)-currentSpeed)>1){
+        get('deltaSpeed').style.backgroundColor='red'
+        get('deltaSpeed').style.color='white'
+    } else {
+        get('deltaSpeed').style.background="none"
+        get('deltaSpeed').style.color='black'
+    }
+
+    let deltaspeed = (currentSpeed/3.6)-lastSpeed;
+    lastSpeed=(currentSpeed/3.6);
+    get('speedAccelOut').innerText=`${(deltaspeed*4).toFixed(2)}`
+    get('speedAccelOutG').innerText=`${((deltaspeed*4)/9.81).toFixed(2)}`
+},250)
+
+
+function DISTANCE_UPDATE(){
+    currentDistance+=(currentSpeed*delta)/50/3600*1000
+    if(true){
+        currentPOS+=(currentSpeed*delta)/50/3600*1000
+    } else {
+        currentPOS-=(currentSpeed*delta)/50/3600*1000
+    }
+
+    get('currentPMout').innerText=`${currentPOS.toFixed(1)}`
+}
+
+let GlobalImg=[{path: "./src/modern_0.png", id:"tunnelimg", div:2.4},
+    {path: "./src/TRAIN.png", id:"trainimg", div:2}]
+let LoadedImg={}
+for(let img of GlobalImg){
+    let imgclass = new Image();
+    imgclass.src = img.path;
+    imgclass.div = img.div;
+    imgclass.onload = () => {
+        LoadedImg[img.id]=imgclass
+    };
+}
+
+
+function BACKGROUND_FILL(){
+    let WallList=[]
+    WallList.push({xs: (0-currentPOS*50), xe:(0-currentPOS*50)+(LoadedImg.tunnelimg.width/LoadedImg.tunnelimg.div)})
+    let recurs = ()=>{
+        if(WallList[WallList.length-1].xe<canvas.width){
+            WallList.push({xs: WallList[WallList.length-1].xe, xe:(LoadedImg.tunnelimg.width/LoadedImg.tunnelimg.div)+WallList[WallList.length-1].xe})
+            recurs()
+        }
+    }
+    recurs()
+    let recurs2 = ()=>{
+        if(WallList[0].xe<0){
+            WallList.shift()
+            recurs2()
+        }
+    }
+    recurs2()
+    for(let wall of WallList){
+        ctx.drawImage(LoadedImg.tunnelimg, wall.xs, 0, (LoadedImg.tunnelimg.width/LoadedImg.tunnelimg.div), (LoadedImg.tunnelimg.height/LoadedImg.tunnelimg.div))
+    }
+    ctx.drawImage(LoadedImg.trainimg, 0, 50, (LoadedImg.trainimg.width/LoadedImg.trainimg.div), (LoadedImg.trainimg.height/LoadedImg.trainimg.div));
+}
+
+function CANVAS_UPDATE(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0,0,canvas.width, canvas.height)
+    if(Object.keys(LoadedImg).length<GlobalImg.length) return;
+    BACKGROUND_FILL()
+}
+
+get('btn_quick_setup').addEventListener('click',()=>{
+    get('enable_physics').checked=true
+    get('auto_physic_radio_elem').checked=true
+    get('train_frott_input').value=0.5
+    get('auto_control_radio_elem').checked=true
+    get("enable_sound").checked=true
+    get('btn_quick_sysstop').disabled=false
+    get('btn_quick_setup').disabled=true
+})
+
+get('btn_quick_sysstop').addEventListener('click',()=>{
+    get('manual_physic_radio_elem').checked=true
+    get('manual_control_radio_elem').checked=true
+    get('btn_quick_sysstop').disabled=true
+    get('btn_quick_setup').disabled=false
+})
+
+get('btn_embrake').addEventListener('click',()=>{
+    fuTriggered=true
+    fuStart=true
+    fuBtn=true
+    get('btn_embrake').disabled=true
+    get('btn_embrake_reset').disabled=false
+})
+get('btn_embrake_reset').addEventListener('click',()=>{
+    fuTriggered=false
+    fuStart=false
+    fuBtn=false
+    get('btn_embrake').disabled=false
+    get('btn_embrake_reset').disabled=true
+})
---
+++ sounds.js
@@ -0,0 +1,384 @@
+const SOUND_MANAGER = {
+    context: false,
+    sounds: {},
+    audios: {},
+    freqPlaying: [],
+    soundscustomvolume: {},
+    soundsdelta: {},
+    globalVolume: 1,
+    copySound: function (originId, ...targetIds) {
+        let originSound = this.sounds[originId];
+        if (!originSound) {
+            console.log("Attempted to copy " + originId + ", but no sound is linked !");
+            return false;
+        }
+        for (let target of targetIds) {
+            this.sounds[target] = originSound;
+            console.log('Copied ' + originId + " to " + target + " !");
+        }
+        return true;
+    },
+    registerSound: function (id, url, customVolume = false) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.responseType = 'arraybuffer';
+        let refThis = this;
+        xhr.onload = function () {
+            refThis.context.decodeAudioData(xhr.response, function (buffer) {
+                if (typeof id === "string") {
+                    id = [id];
+                }
+                for (let tid of id) {
+                    refThis.sounds[tid] = buffer;
+                    refThis.audios[tid] = [];
+                    if (customVolume) {
+                        refThis.soundscustomvolume[tid] = customVolume;
+                    }
+                }
+                //console.log("Registered sound " + id + " !");
+            }, err => {
+                console.error('Unable to register ' + id + '(' + url + '): ', err);
+            });
+        };
+        xhr.send();
+    },
+    playSound: function (id, vol = 1, pitch = 1, onend = false, onnearend = false, prespitch=false) { // Returns true if no sound is linked, meaning should retry later
+        let snd = this.sounds[id];
+        if (!snd) {
+            console.log("Attempted to play " + id + ", but no sound is linked !");
+            return false;
+        }
+        let src = this.context.createBufferSource();
+        src.buffer = snd;
+        // src.playbackRate.value = pitch;
+        this.setPitch(id, src, pitch,prespitch);
+
+        // src.connect(this.context.destination);
+
+        let gainNode = this.context.createGain();
+        src.connect(gainNode);
+        src.gainNode = gainNode;
+        gainNode.connect(this.context.destination);
+        let cstm = this.soundscustomvolume[id] ? this.soundscustomvolume[id] : 1;
+        gainNode.gain.value = this.globalVolume * vol * cstm;
+        // console.log('Gain: '+gainNode.gain.value);
+
+
+        let rthis = this;
+        src.onended = () => {
+            if (onend && typeof onend == "function") onend();
+            let c = 0;
+            for (let ad of rthis.audios[id]) {
+                if (ad === src) {
+                    rthis.audios[id].splice(c, 1);
+                }
+                c++;
+            }
+        }
+        src.ontimeupdate = () => {
+            if (src.currentTime > (src.buffer.duration - .5)) {
+                if (onnearend) onnearend();
+            }
+        }
+        src.start(0);
+        this.audios[id].push(src);
+        return src;
+    },
+    /**
+     * Stops every sound playing on given id, and avoid playing the onend
+     * @param id registered ID of the sound
+     */
+    stopSound: function (id) {
+        for (let idd in this.audios) {
+            if (idd === id) {
+                while (this.audios[id].length > 0) {
+                    let a = this.audios[id].shift();
+                    a.onended = () => {
+                    };
+                    a.stop();
+                    console.log('stopped '+id)
+                }
+            }
+        }
+    },
+    stopAllSound: function () {
+        for (let idd in this.audios) {
+            while (this.audios[idd].length > 0) {
+                let a = this.audios[idd].shift();
+                a.onended = () => {
+                };
+                a.stop();
+                console.log('stopped '+idd)
+            }
+        }
+    },
+    /**
+     * Stops every sound playing on given id, but plays the onend
+     * @param id registered ID of the sound
+     */
+    endSound: function (id) {
+        for (let idd in this.audios) {
+            if (idd === id) {
+                while (this.audios[id].length > 0) {
+                    let a = this.audios[id].shift();
+                    a.stop();
+                }
+            }
+        }
+    },
+    playFreq: function(hz, vol){
+        // vol = Math.min((volRef-vol)/volRef, 1) * (volMult);
+        // -3db = /2
+        // -6db = /4
+        // -9db = /8
+        // etc
+        let volMult = 2
+        vol = 1 / Math.pow(2, (vol)/3);
+        if(vol>1){
+            console.error("Volume to high ! "+vol);
+            return;
+        }
+        vol *= volMult;
+
+        if(this.freqPlaying[hz]){
+            this.freqPlaying[hz].gainNode.gain.value = vol;
+            return;
+        }
+
+        //console.log(this.context)
+
+        let oscillator = this.context.createOscillator();
+        let gainNode = this.context.createGain();
+        let convolver = this.context.createConvolver();
+
+        oscillator.type = 'sine';
+        oscillator.frequency.value = hz;
+        oscillator.connect(gainNode);
+        oscillator.gainNode = gainNode;
+        oscillator.convolverNode = convolver;
+
+        gainNode.connect(/*convolver*/this.context.destination);
+        gainNode.gain.value = vol;
+
+        // convolver.connect(this.context.destination);
+        // convolver.gain.value = vol;
+
+        //this.freqPlaying[hz] = oscillator;
+
+        oscillator.start();
+
+        //console.log('bip')
+    },
+    stopFreq: function(hz){
+        if(this.freqPlaying[hz]){
+            this.freqPlaying[hz].stop();
+            delete this.freqPlaying[hz];
+        }
+    },
+    getPlayingSounds: function (id) {
+        for (let idd in this.audios) {
+            if (idd === id) {
+                return this.audios[idd];
+            }
+        }
+        return false;
+    },
+    isRegistered: function (id) {
+        for (let idd in this.sounds) {
+            if (idd === id) {
+                return true;
+            }
+        }
+        return false;
+    },
+    playBlob: async function (blob, volumeImmune = true) {
+        let src = this.context.createBufferSource();
+        src.buffer = await blob.arrayBuffer();
+
+        let gainNode = this.context.createGain();
+        src.connect(gainNode);
+        src.gainNode = gainNode;
+        gainNode.connect(this.context.destination);
+        if (volumeImmune) gainNode.gain.value = this.globalVolume;
+
+        src.start(0);
+        return src;
+    },
+    loopSound: function(id, vol = 1, pitch = 1, prespitch = false){
+        if(this.getPlayingSounds(id) && this.getPlayingSounds(id).length >= 1) {
+            for(let sound of this.getPlayingSounds(id)){
+                sound.gainNode.gain.value = vol;
+                this.setPitch(id, sound, pitch, prespitch);
+            }
+            return;
+        }
+        let b = ()=>{
+            this.playSound(id,vol,pitch,b);
+        }
+        b();
+    },
+    setPitch: (name, source, value, preserve=false) =>{
+        if((value <0 && !SOUND_MANAGER.soundsdelta[name]) || (value >=0 && !!SOUND_MANAGER.soundsdelta[name])){
+            let a = source.buffer;
+            Array.prototype.reverse.call( a.getChannelData(0) );
+            if(value < 0){
+                SOUND_MANAGER.soundsdelta[name]=true;
+            }else{
+                SOUND_MANAGER.soundsdelta[name]=false;
+            }
+        }
+        source.preservesPitch = true;
+        // source.detune = 800;
+        source.playbackRate.value = Math.abs(value);
+    }
+    
+}
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+SOUND_MANAGER.context = new AudioContext();
+
+
+
+
+
+
+
+
+
+
+
+
+let fu = false
+
+let fuTriggered = false
+
+let fuAcq = false
+let finFu = false
+
+
+
+window.updateSounds=()=>{
+    let throttlePourcent = (currentThrottle*100)/maxThrottle
+
+    let vitessePourcent = (currentSpeed*100)/maxSpeed
+    let aigFreqThreshold = 24 //valeur de viteesse où la fréquence aigue commence à disparaitre
+    let aigFreqVol = 0.5
+    let bFreqVol = 0.1
+    
+    bFreqVol=0.5-aigFreqVol+0.1
+
+    if(fuTriggered===false){
+        if(currentSpeed>aigFreqThreshold){
+            if(currentSpeed>24) aigFreqVol=0.5
+            if(currentSpeed>26) aigFreqVol=0.4
+            if(currentSpeed>30) aigFreqVol=0.3
+            if(currentSpeed>36) aigFreqVol=0.2
+            if(currentSpeed>38) aigFreqVol=0.1
+            if(currentSpeed>40) aigFreqVol=0
+        }
+
+        if(currentSpeed>0){
+            //SOUND_MANAGER.loopSound('hach206',0.5)
+            //SOUND_MANAGER.loopSound('hach206base',0.1)
+        } else {
+            SOUND_MANAGER.stopSound('hach206')
+            SOUND_MANAGER.stopSound('hach206bis')
+            SOUND_MANAGER.stopSound('hach206ng')
+            SOUND_MANAGER.stopSound('hach206all')
+        }
+    
+        if((currentSpeed>0 && !(currentThrottle===0))){
+            SOUND_MANAGER.loopSound('hach206',Math.abs(throttlePourcent)/70)
+            SOUND_MANAGER.loopSound('hach206all',Math.abs(throttlePourcent)/180+0.15)
+            //console.log(`${Math.abs(throttlePourcent)/180+0.15}`)
+            //SOUND_MANAGER.loopSound('hach206ng',0.3)
+            //SOUND_MANAGER.loopSound('hach206bis',0.1)
+            isPlayingHach=true
+        } 
+        if (currentSpeed===0 || currentThrottle===0){
+            //SOUND_MANAGER.stopSound('hach206')
+        }
+
+        if(currentSpeed>0){
+            //SOUND_MANAGER.loopSound('mot206',0.5,currentSpeed/20)
+            SOUND_MANAGER.loopSound('mot2061F',Math.min(20/currentSpeed,0.5),currentSpeed/20)
+            //console.log(`${Math.min(20/currentSpeed,0.5)}`)
+            SOUND_MANAGER.loopSound('mot2062F',aigFreqVol,currentSpeed/20)
+            SOUND_MANAGER.loopSound('mot2063F',bFreqVol,currentSpeed/20)
+        }
+    } else {
+        if(currentSpeed===0) fuTriggered=false;
+
+        fuAcq=true
+        if(fu===false){
+            SOUND_MANAGER.playSound('fuprem206')
+            fu=true
+        }
+        //rangeInput.value=-5
+        //currentSpeed += ((-7.5 / 60) * delta);
+
+        //SOUND_MANAGER.stopSound('mot2062F')
+        SOUND_MANAGER.stopSound('mot2063F')
+        SOUND_MANAGER.stopSound('hach206')
+        SOUND_MANAGER.stopSound('hach206ng')
+        SOUND_MANAGER.stopSound('hach206all')
+
+        
+        SOUND_MANAGER.loopSound('mot2061F',0.5,currentSpeed/20)
+        SOUND_MANAGER.loopSound('mot2062F',0.2,currentSpeed/20)
+
+        if(currentSpeed<12 && finFu===false){
+            finFu=true
+            SOUND_MANAGER.playSound('finfu206',1.5)
+        }
+
+
+        //SOUND_MANAGER.loopSound('mot2063F',bFreqVol,currentSpeed/20)
+    }
+
+    if(currentSpeed>0 && fu===true && fuTriggered===false){
+        fu=false
+        fuAcq=false
+        finFu=false
+        SOUND_MANAGER.playSound('defu206')
+        //SOUND_MANAGER.stopSound('ambiance206')
+        //SOUND_MANAGER.stopSound('finHach206')
+    } else if (currentSpeed===0 && fu===false && fuAcq===false){
+        fu=true
+        SOUND_MANAGER.playSound('fu206')
+        //SOUND_MANAGER.loopSound('ambiance206')
+        SOUND_MANAGER.stopSound('finfu206')
+    }
+
+    if(currentSpeed>0){
+        SOUND_MANAGER.loopSound('ambBase206',currentSpeed/60,Math.max((currentSpeed/60)-0.3,0.1))
+        //console.log(`${Math.max((currentSpeed/60)-0.3,0.1)}`)
+    }
+}
+
+
+
+
+
+
+
+(()=>{
+    //SOUND_MANAGER.registerSound('hach206','./snd/val206/hacheur1.mp3')
+    SOUND_MANAGER.registerSound('hach206bis','./src/snd/val206/hacheur1.mp3')
+    SOUND_MANAGER.registerSound('hach206','./src/snd/val206/hacheureel.mp3')
+    SOUND_MANAGER.registerSound('hach206all','./src/snd/val206/hachAig2.mp3')
+    SOUND_MANAGER.registerSound('hach206base','./src/snd/val206/hacheur_base.mp3')
+    SOUND_MANAGER.registerSound('hach206ng','./src/snd/val206/hacheurng.mp3')
+    SOUND_MANAGER.registerSound('ambiance206','./src/snd/val206/ambianceinter.mp3')
+    SOUND_MANAGER.registerSound('ambBase206','./src/snd/val206/ambAvecBase.mp3')
+    SOUND_MANAGER.registerSound('ambSansBase206','./src/snd/val206/ambSansBase.mp3')
+    SOUND_MANAGER.registerSound('finHach206','./src/snd/val206/finHach.mp3')
+    SOUND_MANAGER.registerSound('mot206','./src/snd/val206/mot.mp3')
+    SOUND_MANAGER.registerSound('mot2061F','./src/snd/val206/mot1F.mp3')
+    SOUND_MANAGER.registerSound('mot2062F','./src/snd/val206/mot2F.mp3')
+    SOUND_MANAGER.registerSound('mot2063F','./src/snd/val206/mot3F.mp3')
+
+    SOUND_MANAGER.registerSound('fu206','./src/snd/val206/fu-propre.mp3')
+    SOUND_MANAGER.registerSound('defu206','./src/snd/val206/de-fu.mp3')
+    SOUND_MANAGER.registerSound('fuprem206','./src/snd/val206/fu_prem.mp3')
+    SOUND_MANAGER.registerSound('finfu206','./src/snd/val206/finFu.mp3')
+})();
